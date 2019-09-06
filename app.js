b4562d2f153ece5dd0404738fa7383408f2763c5
Chetan kumar <chetansain886@gmail.com>
2019-09-06 11:23:09 +0000

    Add restaurant routes

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var resturantRouter = require('./routes/restaurant');
 
 var app = express();
 
@@ -20,6 +21,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/restaurants', indexRouter);
+app.use('/restaurant', resturantRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
